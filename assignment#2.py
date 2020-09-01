df189ff89206d0b6f4b21226185243e4616e0a08
Pia Sinivaara <66398959+Pia-Sini@users.noreply.github.com>
2020-09-01 04:05:26 +0300

    Add files via upload
    Final

--- assignment#2.py
+++ assignment#2.py
@@ -1,14 +1,18 @@
 import re
 import statistics
-s = input("Anna lista numeroita: "s)
+s = input("Anna lista numeroita: ")
 result = [int(d) for d in re.findall(r'-?\d+', s)]
 
 #sum
 print ("Sum:    ",sum(result))
 
 #mean
-print ("Mean:   ",str(round(statistics.mean(result), 1)))
+mean1=str(statistics.mean(result))
+mean1=float(mean1)
+print ("Mean:   ",str(round(mean1, 1)))
 
 #median
-print ("Median: ",str(round(statistics.median(result), 1)))
+median1=str(statistics.median(result))
+median1=float(median1)
+print ("Median: ",str(round(median1, 1)))
 
